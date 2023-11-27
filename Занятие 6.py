ad063360446172bac0bde13ab08ee5c402187098
RadNika <Nika_Sally@mail.ru>
2023-11-27 19:59:48 +1000

    lesson6

---
+++ Занятие 6.py
@@ -0,0 +1,8 @@
+import requests
+response = requests.get('https://www.vk.com')
+print(requests.get('https://www.vk.com'))
+print(requests.post('https://www.vk.com'))
+print(requests.put('https://www.vk.com'))
+print(requests.delete('https://www.vk.com'))
+
+
